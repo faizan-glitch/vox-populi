2ac76efd4a9a8ade644d8722a8e9e4c735ca48a7
faizan <faizan-666@outlook.com>
2021-10-21 13:37:48 +0500

    changed name

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -6,7 +6,7 @@
 <div class="hero min-h-full bg-base-200 block md:grid">
 	<div class="flex-col justify-center hero-content lg:flex-row m-2">
 		<div class="text-center lg:text-left hidden lg:block">
-			<h1 class="mb-5 text-5xl font-bold">Simple Poll</h1>
+			<h1 class="mb-5 text-5xl font-bold">Vox Populi</h1>
 			<p class="mb-5">
 				A simple Polls and Surveys app created by Faizan over the weekend with the purpose of
 				learning SvelteKit, DaisyUI and Tailwind CSS.
